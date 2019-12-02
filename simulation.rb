abf59c09eab7a5613adda9c9c689733fc9424f97
Beth Ashton <Mbeth.ashton@gmail.com>
2019-12-02 11:37:20 -0500

    Add blob class
    Need help with tests

--- simulation.rb
+++ simulation.rb
@@ -1,22 +1,36 @@
 # frozen_string_literal: true
 
+class Blob
+  def initialize(coords, speed)
+    @coordinates = [coords[0], coords[1]]
+    @speed = speed
 
-def move(length, angle, coordinates)
-  radians = angle * (Math::PI*2)
-  old_x = coordinates[0]
-  old_y = coordinates[1]
+    puts "New Blob Coords: #{@coordinates[0]}, #{@coordinates[1]}"
+  end
 
-  delta_x = (Math.cos(radians) * length).round(4)
-  delta_y = (Math.sin(radians) * length).round(4)
+  def move
+    angle = rand
+    find_new_coords(@speed, angle, @coordinates)
+  end
 
-  new_x = old_x + delta_x
-  new_y = old_y + delta_y
+  def find_new_coords(length, angle, coordinates)
+    radians = angle * (Math::PI * 2)
+    old_x = coordinates[0]
+    old_y = coordinates[1]
 
-  puts "You move #{length} units at an angle of #{radians} radians"
-  puts "Starting: #{old_x}, #{old_y}; Deltas: #{delta_x}, #{delta_y}"
-  puts "new coords: #{new_x}, #{new_y}"
-  coordinates = [new_x, new_y]
+    delta_x = (Math.cos(radians) * length).round(4)
+    delta_y = (Math.sin(radians) * length).round(4)
+
+    new_x = old_x + delta_x
+    new_y = old_y + delta_y
+
+    puts "You move #{length} units at an angle of #{radians} radians"
+    puts "Starting: #{old_x}, #{old_y}; Deltas: #{delta_x}, #{delta_y}"
+    puts "new coords: #{new_x}, #{new_y}"
+    [new_x, new_y]
+  end
 end
 
 puts 'Congrats! You are running the simulation.'
-move(1, rand(), [1,1])
+blob = Blob.new([2, 2], 1)
+blob.move
